01aad62f133c0d67ca7f57d612a46252fea4ffd8
Adam Bird <adam.bird@gmail.com>
2012-05-03 11:56:16 +0100

    added some error handling to Controller#notificon_tracker

--- lib/notificon/controller.rb
+++ lib/notificon/controller.rb
@@ -5,6 +5,8 @@ module Notificon
         read_at = (respond_to?(:current_time) && current_time) || Time.now
         Notificon.mark_notification_read(params[Notificon.notification_id_param], read_at)
       end
+    rescue => e
+      Notificon.logger.error { "Notificon::Controller#notificon_tracker error processing notification params - #{params.inspect} error - #{e.inspect}" }
     end
   end
 end
