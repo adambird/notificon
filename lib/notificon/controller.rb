7373d11643a82a5610a37e23f13f10c569850cee
Adam Bird <adam.bird@gmail.com>
2012-05-24 15:05:33 +0100

    added naive true is something updated

--- lib/notificon/controller.rb
+++ lib/notificon/controller.rb
@@ -9,9 +9,10 @@ module Notificon
   module Controller
     # Public : method to use as a before filter on the application controller to track read status
     # of notifications
+    #
+    # Returns bookean indicating whether updates made
     def notificon_tracker
-      _track_explicit_read
-      _track_implicit_read
+      _track_explicit_read | _track_implicit_read
     rescue => e
       Notificon.logger.error { "Notificon::Controller#notificon_tracker error processing notification params - #{params.inspect} error - #{e.inspect}" }
     end
@@ -20,8 +21,10 @@ module Notificon
     def _track_explicit_read
       if params[Notificon.notification_item_id_param] && params[Notificon.notification_username_param]
         Notificon.mark_all_read_for_item(params[Notificon.notification_username_param], params[Notificon.notification_item_id_param], _notificon_read_at)
+        true
       elsif id = params[Notificon.notification_id_param]
         Notificon.mark_notification_read(id, _notificon_read_at)
+        true
       end
     end
 
@@ -29,6 +32,7 @@ module Notificon
     def _track_implicit_read
       if respond_to?(:current_username) && respond_to?(:current_item_id) && current_username && current_item_id
         Notificon.mark_all_read_for_item(current_username, current_item_id, _notificon_read_at)
+        true
       end
     end
 
