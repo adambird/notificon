17461d1b04821a14e992b910662aa76a22dd5d30
Adam Bird <adam.bird@gmail.com>
2012-05-24 12:45:10 +0100

    added implicit tracking support for notifications

--- lib/notificon/controller.rb
+++ lib/notificon/controller.rb
@@ -1,16 +1,40 @@
+# Public : Module to include in ApplicationController to support notification read tracking
+#
+# class ApplicationController
+#   include Notificon::Controller
+#   before_filter :notificon_tracker
+#
+# end
 module Notificon
   module Controller
+    # Public : method to use as a before filter on the application controller to track read status
+    # of notifications
     def notificon_tracker
-      if id = params[Notificon.notification_id_param]
-        read_at = (respond_to?(:current_time) && current_time) || Time.now
-        if params[Notificon.notification_item_id_param] && params[Notificon.notification_username_param]
-          Notificon.mark_all_read_for_item(params[Notificon.notification_username_param], params[Notificon.notification_item_id_param], read_at)
-        else
-          Notificon.mark_notification_read(id, read_at)
-        end
-      end
+      _track_explicit_read
+      _track_implicit_read
     rescue => e
       Notificon.logger.error { "Notificon::Controller#notificon_tracker error processing notification params - #{params.inspect} error - #{e.inspect}" }
+    end
+
+    # Private : uses notificon explicit parameters to track reads for items
+    def _track_explicit_read
+      if params[Notificon.notification_item_id_param] && params[Notificon.notification_username_param]
+        Notificon.mark_all_read_for_item(params[Notificon.notification_username_param], params[Notificon.notification_item_id_param], _notificon_read_at)
+      elsif id = params[Notificon.notification_id_param]
+        Notificon.mark_notification_read(id, _notificon_read_at)
+      end
+    end
+
+    # Private : uses controller methods to track implicit reads for items
+    def _track_implicit_read
+      if respond_to?(:current_username) && respond_to?(:current_item_id) && current_username && current_item_id
+        Notificon.mark_all_read_for_item(current_username, current_item_id, _notificon_read_at)
+      end
+    end
+
+    # Private : lazy assignment of read_at  time
+    def _notificon_read_at
+      @_notificon_read_at ||= ((respond_to?(:current_time) && current_time) || Time.now)
     end
   end
 end
--- spec/notificon/controller_spec.rb
+++ spec/notificon/controller_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 class DummyController
   include Notificon::Controller
-  
+
   def params
     @_params ||= {}
   end
-  
+
 end
 
 describe Controller do
@@ -16,20 +16,20 @@ describe Controller do
       @time = random_time
       @controller.stub(:current_time) { @time }
     end
-    
+
     subject { @controller.notificon_tracker }
-    
+
     context "when id param passed" do
       before(:each) do
         @id = random_string
         @controller.params[Notificon.notification_id_param] = @id
       end
-      
+
       it "marks the notification as read" do
         Notificon.should_receive(:mark_notification_read).with(@id, @time)
         subject
       end
-      
+
       context "when item_id param passed" do
         before(:each) do
           @item_id = random_string
@@ -47,10 +47,23 @@ describe Controller do
         end
       end
     end
-    
+
     context "when id param not passed" do
       it "does not attempt to mark notification" do
         Notificon.should_not_receive(:mark_notification_read)
+        subject
+      end
+    end
+
+    context "when controller supports current_username and current_item_id" do
+      before(:each) do
+        @current_username = random_string
+        @current_item_id = random_string
+        @controller.stub(:current_username) { @current_username }
+        @controller.stub(:current_item_id) { @current_item_id }
+      end
+      it "marks all the notifications for the current_item_id read" do
+        Notificon.should_receive(:mark_all_read_for_item).with(@current_username, @current_item_id, @time)
         subject
       end
     end
